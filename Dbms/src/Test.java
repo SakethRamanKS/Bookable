e42a8baf781cdf81320e80f84e64b5c10dcf5832
BattleBaxx <susanth1503@gmail.com>
2020-09-19 13:23:05 +0530

    Susanth commit

--- Dbms/src/Test.java
+++ Dbms/src/Test.java
@@ -3,7 +3,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello world again just to make sure this thing is working properly");
 	}
 
 }
